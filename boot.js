9030edf74414bbdd0dff01188bf51e33b64559aa
alihxn <91802177+alyhxn@users.noreply.github.com>
2025-07-26 19:24:02 -0600

    Update boot.js

--- boot.js
+++ boot.js
@@ -1,2 +1,5 @@
-import '/temp/shim.js'
-boot(import.meta.url)
+const env = { version: 'latest' }
+const arg = { x: 321, y: 543 }
+const url = 'http://playproject.io/datashell/shim.js'
+const src = `${url}?${new URLSearchParams(env)}#${new URLSearchParams(arg)}`
+this.open ? document.body.append(Object.assign(document.createElement('script'), { src })) : importScripts(src)
